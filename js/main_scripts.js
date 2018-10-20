357ad4855741ef6bae2535d4c84846ff371a902e
sergiosmarinho <sergio.marinho94@gmail.com>
2018-10-20 04:19:59 -0300

    patterns in classes and ids, unused files removed

--- js/main_scripts.js
+++ js/main_scripts.js
@@ -1,22 +1,22 @@
 window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("Buttontop").style.display = "block";
-    } else {
-        document.getElementById("Buttontop").style.display = "none";
-    }
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("button-top").style.display = "block";
+  } else {
+      document.getElementById("button-top").style.display = "none";
+  }
 }
 
 function scrollToFunction(sectionToScroll, close) {
-    if($(window).width() < 780 && close==1){
-        $('.navbar-toggler').click();
-    }
-    pos = $(sectionToScroll).position().top;
-    window.scroll({
-      top: pos, 
-      left: 0, 
-      behavior: 'smooth' 
-    });
+  if($(window).width() < 780 && close==1){
+      $('.navbar-toggler').click();
+  }
+  pos = $(sectionToScroll).position().top;
+  window.scroll({
+    top: pos, 
+    left: 0, 
+    behavior: 'smooth' 
+  });
 }
 
 function isInScreen(elementName) {
